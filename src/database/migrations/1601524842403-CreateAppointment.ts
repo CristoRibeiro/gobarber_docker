a066a81bb3f3acbd09ce101d49f64cdf22c56195
CristoRibeiro <cristianosrsilva@gmail.com>
2020-10-01 01:17:45 -0300

    AddMigrations

---
+++ src/database/migrations/1601524842403-CreateAppointment.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+class CreateAppointment1601524842403 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          { name: 'provider', type: 'varchar', isNullable: false },
+          {
+            name: 'date',
+            type: 'timestamp with time zone ',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
+
+export default CreateAppointment1601524842403;
